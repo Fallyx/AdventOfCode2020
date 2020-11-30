9d557572cb90dd742bccad0e7ecc7e982c1528d0
Janis <Fallyx@users.noreply.github.com>
2020-11-30 22:01:17 +0100

    Changed main method

--- Program.cs
+++ Program.cs
@@ -5,16 +5,30 @@ namespace AdventOfCode2020
 {
     class Program
     {
+        private static Stopwatch swTot = new Stopwatch();
+        private static Stopwatch swDay = new Stopwatch();
+
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Run a single day [1-25] or [a]ll:");
+            String input = Console.ReadLine();
+
+            if (input == "a") {
+                FullRun();
+            } 
+
+            int day;
+            bool success = int.TryParse(input, out day);
+
+            if (!success) {
+                return;
+            }
+
+            SingleRun(day);
         }
 
         private static void FullRun() 
         {
-            Stopwatch swTot = new Stopwatch();
-            Stopwatch swDay = new Stopwatch();
-
             swTot.Start();
 
             #region day 1
@@ -24,14 +38,25 @@ namespace AdventOfCode2020
             Console.WriteLine($"Day 01 elapsed time: {swDay.Elapsed}\n");
             #endregion
 
-
             swTot.Stop();
             Console.WriteLine($"\nTotal elapsed time: {swTot.Elapsed}");
         }
 
-        private static void SingleRun() 
+        private static void SingleRun(int day) 
         {
+            swDay.Start();
 
+            switch (day)
+            {
+                case 1:
+                    Day01.Day01.Task1();
+                    break;  
+                default:
+                    break;
+            }
+
+            swDay.Stop();
+            Console.WriteLine($"Day {day.ToString("D2")} elapsed time: {swDay.Elapsed}\n");
         }
     }
 }
