fb49066af2eb9411b5ea53d0b026e52277863878
Janis <Fallyx@users.noreply.github.com>
2020-12-01 08:08:19 +0100

    Solved Day 1

--- Day01/Day01.cs
+++ Day01/Day01.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace AdventOfCode2020.Day01
 {
     class Day01
@@ -5,7 +9,44 @@ namespace AdventOfCode2020.Day01
         const string inputPath = @"Day01/Input.txt";
         public static void Task1()
         {
-            
+            List<int> numbers = new List<int>();
+            bool foundFirst = false;
+            bool foundSecond = false;
+
+            using (StreamReader reader = new StreamReader(inputPath)) 
+            {
+                string line;
+                while((line = reader.ReadLine()) != null) 
+                {
+                    numbers.Add(int.Parse(line));
+                }
+            }
+
+            for (int i = 0; i < numbers.Count - 1; i++) 
+            {
+                for (int x = 1; x < numbers.Count; x++) 
+                {
+                    if (!foundFirst && numbers[i] + numbers[x] == 2020) 
+                    {
+                        Console.WriteLine($"Task1: {numbers[i] * numbers[x]}");
+                        foundFirst = true;
+                    }
+
+                    for(int y = 2; y < numbers.Count; y++) 
+                    {
+                        if (!foundSecond && numbers[i] + numbers[x] + numbers[y] == 2020)
+                        {
+                            Console.WriteLine($"Task 2: {numbers[i] * numbers[x] * numbers[y]}");
+                            foundSecond = true;
+                        }
+
+                        if (foundFirst && foundSecond) 
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
         }
     }    
 }
