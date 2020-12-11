fddd0680bf963ad9c67b1b19f70f945c4ca91936
Janis <Fallyx@users.noreply.github.com>
2020-12-11 18:47:36 +0100

    Day 11 refactoring

--- Day11/Day11.cs
+++ Day11/Day11.cs
@@ -25,56 +25,11 @@ namespace AdventOfCode2020.Day11
 
             char[,] prevTask2 = prev.Clone() as char[,];
 
-            Task1(prev);
-            Task2(prevTask2);
+            Seats(prev, 4);
+            Seats(prev, 5, false);
         }
 
-        private static void Task1(char[,] prev)
-        {
-            char[,] next = prev.Clone() as char[,];
-            bool changed;
-
-            do
-            {
-                changed = false;
-
-                for (int y = 0; y < prev.GetLength(0); y++)
-                {
-                    for (int x = 0; x < prev.GetLength(1); x++)
-                    {
-                        int adjacentOccupied = 0;
-
-                        if (y - 1 >= 0 && x - 1 >= 0 && prev[y-1, x-1] == '#') adjacentOccupied++;
-                        if (y - 1 >= 0 && prev[y-1, x] == '#') adjacentOccupied++;
-                        if (y - 1 >= 0 && x + 1 < prev.GetLength(1) && prev[y-1, x+1] == '#') adjacentOccupied++;
-
-                        if (x - 1 >= 0 && prev[y, x-1] == '#') adjacentOccupied++;
-                        if (x + 1 < prev.GetLength(1) && prev[y, x+1] == '#') adjacentOccupied++;
-
-                        if (y + 1 < prev.GetLength(0) && x - 1 >= 0 && prev[y+1, x-1] == '#') adjacentOccupied++;
-                        if (y + 1 < prev.GetLength(0) && prev[y+1,x] == '#') adjacentOccupied++;
-                        if (y + 1 < prev.GetLength(0) && x + 1 < prev.GetLength(1) && prev[y+1,x+1] == '#') adjacentOccupied++;
-
-                        if (prev[y, x] == 'L' && adjacentOccupied == 0) 
-                        {
-                            next[y, x] = '#';
-                            changed = true;
-                        }
-                        else if (prev[y, x] == '#' && adjacentOccupied >= 4)
-                        {
-                            next[y, x] = 'L';
-                            changed = true;
-                        }
-                    }
-                }
-
-                prev = next.Clone() as char[,];
-            } while(changed);
-
-            Console.WriteLine($"Task 1: {prev.Cast<char>().ToArray().Count(c => c == '#')}");
-        }
-
-        private static void Task2(char[,] prev)
+        private static void Seats(char[,] prev, int freeSeat, bool onlyAdj = true)
         {
             char[,] next = prev.Clone() as char[,];
             bool changed;
@@ -94,61 +49,101 @@ namespace AdventOfCode2020.Day11
                         while(found.Any(f => f == false))
                         {
                             if (!found[0] && (y - searchRadius < 0 || x - searchRadius < 0)) found[0] = true;
-                            if (!found[0] && (prev[y-searchRadius, x-searchRadius] == '#' || prev[y-searchRadius, x-searchRadius] == 'L')) 
+                            if (!found[0]) 
                             {
-                                if (prev[y-searchRadius, x-searchRadius] == '#') adjacentOccupied++;
-                                found[0] = true;
+                                if (prev[y-searchRadius, x-searchRadius] == '#') 
+                                {
+                                    adjacentOccupied++;
+                                    found[0] = true;
+                                }
+                                else if (prev[y-searchRadius, x-searchRadius] == 'L' || onlyAdj) 
+                                    found[0] = true;
                             } 
 
                             if (!found[1] && y - searchRadius < 0) found[1] = true;
-                            if (!found[1] && (prev[y-searchRadius, x] == '#' || prev[y-searchRadius, x] == 'L')) 
+                            if (!found[1]) 
                             {
-                                if (prev[y-searchRadius, x] == '#') adjacentOccupied++;
-                                found[1] = true;
+                                if (prev[y-searchRadius, x] == '#') 
+                                {
+                                    adjacentOccupied++;
+                                    found[1] = true;
+                                }
+                                else if (prev[y-searchRadius, x] == 'L' || onlyAdj)
+                                    found[1] = true;
                             }
 
                             if (!found[2] && (y - searchRadius < 0 || x + searchRadius >= prev.GetLength(1))) found[2] = true;
-                            if (!found[2] && (prev[y-searchRadius, x+searchRadius] == '#' || prev[y-searchRadius, x+searchRadius] == 'L')) 
+                            if (!found[2]) 
                             {
-                                if (prev[y-searchRadius, x+searchRadius] == '#') adjacentOccupied++;
-                                found[2] = true;
+                                if (prev[y-searchRadius, x+searchRadius] == '#')
+                                {
+                                    adjacentOccupied++;
+                                    found[2] = true;
+                                }
+                                else if (prev[y-searchRadius, x+searchRadius] == 'L' || onlyAdj)
+                                    found[2] = true;
                             }
 
 
                             if (!found[3] && x - searchRadius < 0) found[3] = true;
-                            if (!found[3] && (prev[y, x-searchRadius] == '#' || prev[y, x-searchRadius] == 'L')) 
+                            if (!found[3]) 
                             {
-                                if (prev[y, x-searchRadius] == '#') adjacentOccupied++;
-                                found[3] = true;
+                                if (prev[y, x-searchRadius] == '#') 
+                                {
+                                    adjacentOccupied++;
+                                    found[3] = true;
+                                }
+                                else if (prev[y, x-searchRadius] == 'L' || onlyAdj)
+                                    found[3] = true;
                             }
 
                             if (!found[4] && x + searchRadius >= prev.GetLength(1)) found[4] = true;
-                            if (!found[4] && (prev[y, x+searchRadius] == '#' || prev[y, x+searchRadius] == 'L')) 
+                            if (!found[4]) 
                             {
-                                if (prev[y, x+searchRadius] == '#') adjacentOccupied++;
-                                found[4] = true;
+                                if (prev[y, x+searchRadius] == '#') 
+                                {
+                                    adjacentOccupied++;
+                                    found[4] = true;
+                                }
+                                else if (prev[y, x+searchRadius] == 'L' || onlyAdj)
+                                    found[4] = true;
                             }
 
 
                             if (!found[5] && (y + searchRadius >= prev.GetLength(0) || x - searchRadius < 0)) found[5] = true;
-                            if (!found[5] && (prev[y+searchRadius, x-searchRadius] == '#' || prev[y+searchRadius, x-searchRadius] == 'L')) 
+                            if (!found[5]) 
                             {
-                                if (prev[y+searchRadius, x-searchRadius] == '#') adjacentOccupied++;
-                                found[5] = true;
+                                if (prev[y+searchRadius, x-searchRadius] == '#') 
+                                {
+                                    adjacentOccupied++;
+                                    found[5] = true;
+                                }
+                                else if (prev[y+searchRadius, x-searchRadius] == 'L' || onlyAdj)
+                                    found[5] = true;
                             }
 
                             if (!found[6] && y + searchRadius >= prev.GetLength(0)) found[6] = true;
-                            if (!found[6] && (prev[y+searchRadius,x] == '#' || prev[y+searchRadius,x] == 'L')) 
+                            if (!found[6]) 
                             {
-                                if (prev[y+searchRadius,x] == '#') adjacentOccupied++;
-                                found[6] = true;
+                                if (prev[y+searchRadius,x] == '#') 
+                                {
+                                    adjacentOccupied++;
+                                    found[6] = true;
+                                }
+                                else if (prev[y+searchRadius,x] == 'L' || onlyAdj)
+                                    found[6] = true;
                             }
 
                             if (!found[7] && (y + searchRadius >= prev.GetLength(0) || x + searchRadius >= prev.GetLength(1))) found[7] = true;
-                            if (!found[7] && (prev[y+searchRadius,x+searchRadius] == '#' || prev[y+searchRadius,x+searchRadius] == 'L')) 
+                            if (!found[7]) 
                             {
-                                if (prev[y+searchRadius,x+searchRadius] == '#') adjacentOccupied++;
-                                found[7] = true;
+                                if (prev[y+searchRadius,x+searchRadius] == '#') 
+                                {
+                                    adjacentOccupied++;
+                                    found[7] = true;
+                                }
+                                else if (prev[y+searchRadius,x+searchRadius] == 'L' || onlyAdj)
+                                    found[7] = true;
                             }
 
                             searchRadius++;
@@ -159,7 +154,7 @@ namespace AdventOfCode2020.Day11
                             next[y, x] = '#';
                             changed = true;
                         }
-                        else if (prev[y, x] == '#' && adjacentOccupied >= 5)
+                        else if (prev[y, x] == '#' && adjacentOccupied >= freeSeat)
                         {
                             next[y, x] = 'L';
                             changed = true;
@@ -168,10 +163,9 @@ namespace AdventOfCode2020.Day11
                 }
 
                 prev = next.Clone() as char[,];
-
             } while(changed);
 
-            Console.WriteLine($"Task 2: {prev.Cast<char>().ToArray().Count(c => c == '#')}");
+            Console.WriteLine($"Task {(onlyAdj ? 1 : 2)}: {prev.Cast<char>().ToArray().Count(c => c == '#')}");
         }
     }
 }
